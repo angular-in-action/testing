f3154afa7d25984e959177bf2e5292ffa3d64003
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-09-09 13:01:41 -0500

    management component test

--- src/app/components/manage/manage.component.spec.ts
+++ src/app/components/manage/manage.component.spec.ts
@@ -1,10 +1,56 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { ManageComponent } from './manage.component';
+import { StocksService } from '../../services/stocks.service';
+import { StubStocksService } from '../../services/stocks.service.stub';
+import { MockSymbolsList } from '../../services/stocks.mock';
 
 describe('ManageComponent', () => {
+  let component: ManageComponent;
+  let fixture: ComponentFixture<ManageComponent>;
+  let el: HTMLElement;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+      ],
+      declarations: [
+        ManageComponent
+      ],
+      providers: [
+        { provide: StocksService, useClass: StubStocksService }
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ManageComponent);
+    component = fixture.componentInstance;
+    el = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add or remove symbols from the list', () => {
+    expect(component.symbols).toEqual(MockSymbolsList);
+    component.stock = 'ABC';
+    component.add();
+    expect(component.symbols).toEqual([...MockSymbolsList, 'ABC']);
+    component.remove('ABC');
+    expect(component.symbols).toEqual(MockSymbolsList);
+  });
+
+  it('should render the template', () => {
+    const items = el.querySelectorAll('td.mdl-data-table__cell--non-numeric');
+
+    for (let i = 0; i < items.length; ++i) {
+      expect(items[i].innerHTML).toContain(MockSymbolsList[i]);
+    }
+  });
 });
