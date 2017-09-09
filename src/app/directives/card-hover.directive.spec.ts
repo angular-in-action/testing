ee963cab04a0c81daeabc7fb34bbff07fa31f22b
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-09-09 13:26:17 -0500

    card-hover directive test

--- src/app/directives/card-hover.directive.spec.ts
+++ src/app/directives/card-hover.directive.spec.ts
@@ -1,5 +1,47 @@
+import { Component, ElementRef } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { CardHoverDirective } from './card-hover.directive';
 
-describe('CardHoverDirective', () => {
+@Component({
+  template: `<div cardHover class="mdl-card decrease">Content</div>`
+})
+class MockComponent {}
 
+describe('CardHoverDirective', () => {
+  let directive: CardHoverDirective;
+  let card: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MockComponent,
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    const fixture = TestBed.createComponent(MockComponent);
+    card = fixture.debugElement.query(By.css('[cardHover]')).nativeElement;
+    directive = new CardHoverDirective(new ElementRef(fixture.debugElement.nativeElement));
+    directive.ngOnInit();
+  });
+
+  it('should apply the correct background colors', () => {
+    expect(card.style.backgroundColor.replace(/ /g, '')).toContain('rgb(255,171,64)');
+    card.classList.remove('decrease');
+    card.classList.add('increase');
+    directive.ngOnInit();
+    expect(card.style.backgroundColor.replace(/ /g, '')).toContain('rgb(63,81,181)');
+    card.classList.remove('increase');
+    directive.ngOnInit();
+    expect(card.style.backgroundColor).toEqual('');
+  });
+
+  it('should apply hover styling', () => {
+    directive.onMouseOver();
+    expect(card.style.top).toEqual('-2px');
+    directive.onMouseOut();
+    expect(card.style.top).toEqual('');
+  });
 });
