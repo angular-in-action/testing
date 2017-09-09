6c9d85b3d758ea1c5e94c9450087bb0b60c959b9
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-09-09 13:26:58 -0500

    delay directive test

--- src/app/directives/delay.directive.spec.ts
+++ src/app/directives/delay.directive.spec.ts
@@ -1,5 +1,45 @@
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture, fakeAsync, async, tick } from '@angular/core/testing';
 import { DelayDirective } from './delay.directive';
 
-describe('DelayDirective', () => {
+@Component({
+  template: `<div *delay="delay"><h1>DELAYED</h1></div>`
+})
+class MockComponent {
+  delay = 10;
+}
 
+describe('DelayDirective', () => {
+  let fixture: ComponentFixture<MockComponent>;
+  let el: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MockComponent,
+        DelayDirective
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockComponent);
+    el = fixture.debugElement.nativeElement;
+  });
+
+  it('should show after the specified delay using fakeAsync', fakeAsync(() => {
+    expect(el.innerHTML).not.toContain('DELAYED');
+    fixture.detectChanges();
+    tick(10);
+    expect(el.innerHTML).toContain('DELAYED');
+  }));
+
+  it('should show after the specified delay using async', async(() => {
+    expect(el.innerHTML).not.toContain('DELAYED');
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(el.innerHTML).toContain('DELAYED');
+    });
+  }));
 });
